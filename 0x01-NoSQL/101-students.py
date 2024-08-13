bc4347c7ccfc1b4d876825a473b292d8a68c48d7
Eng-Phenomena <eng.phenomena@gmail.com>
2024-08-13 18:44:16 +0000

    fin

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''Task 14
+'''
+
+
+def top_students(mongo_collection):
+    '''Prints all students collections sorted by average score.
+    '''
+    student = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return student
